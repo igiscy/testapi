42559a3173d37f3e5f20a72d47027bd717fe0e68
changyang <changyang@earthbook.xyz>
2024-08-23 13:15:28 +0800

    sqlite test

--- api.py
+++ api.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, jsonify
 
+import sqlite3
+
+conn = sqlite3.connect('machine.db') 
+c = conn.cursor()
+
 app = Flask(__name__)
 
 @app.route("/info", methods=["GET"])
-def get_students():
+def get_info():
+    c.execute('select * from register')
+    info = c.fetchall()
+    return {'result':info}
+
+@app.route("/register", methods=["POST"])
+def set_machine():
+    request_data = request.get_json()
+    c.execute('insert into register values ('+ request_data['machine_id']+', '+request_data['machine_name']+')')
+    conn.commit()
     return {'result':True}
 
 
